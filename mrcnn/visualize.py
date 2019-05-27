920890c0ae58e334277ac71bcbad96363f375013
Muhammad Monjurul Karim <40798690+monjurulkarim@users.noreply.github.com>
2019-05-26 19:11:02 -0500

    Update visualize.py

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -1,10 +1,9 @@
 """
-Mask R-CNN
-Display and Visualization Functions.
+Muhammad Monjurul Karim
+PhD Student of Systems Engineering
+Missouri University of Science and Technology
 
-Copyright (c) 2017 Matterport, Inc.
-Licensed under the MIT License (see LICENSE for details)
-Written by Waleed Abdulla
+Code adopted from 2017 Matterport, Inc.
 """
 
 import os
